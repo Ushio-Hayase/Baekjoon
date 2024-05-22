8c15cb32d327bdc39a10f442dba5f0f87791e8f6
UshioHayase <ushiohayase@gmail.com>
2024-05-22 23:44:19 +0900

    1107

--- 1107.cpp
+++ 1107.cpp
@@ -1,20 +1,53 @@
 #include <iostream>
-#include <map>
+#include <string>
 
 using namespace std;
 
-map<char, bool> breaken;
+int broken_btn[10] = {
+    0,
+};
 
-int main() {
-    for(int i = 0; i< 10; i++) breaken.insert(make_pair(i + '0', true));
 
-    int N, M;
-    scanf("%d", &N);
-    scanf("%d", &M);
-
-    for(int i = 0; i < M; i++) {
-        int tmp;
-        scanf("%d", &tmp);
-        breaken[tmp + '0'] = false;
+bool btn_set(int n)
+{
+    string str_n = to_string(n);
+    for (int i = 0; i < str_n.length(); i++)
+    {
+        if (broken_btn[str_n[i] - '0'] == 1)
+            return false;
     }
+
+    return true;
+}
+
+int main()
+{
+    int N;
+    int M;
+    cin >> N;
+    cin >> M;
+
+    int ch = 100;
+    int ans;
+
+    for (int i = 0; i < M; i++)
+    {
+        int btn_number;
+        cin >> btn_number;
+        broken_btn[btn_number] = 1;
+    }
+
+    int cnt = abs(ch - N);
+	
+    for (int i = 0; i <= 1000000; i++)
+    {
+        if (btn_set(i) == true)
+        {
+            int second_cnt = abs(N - i) + to_string(i).length();
+            cnt = min(cnt, second_cnt);
+        }
+    }
+    cout << cnt;
+
+    return 0;
 }

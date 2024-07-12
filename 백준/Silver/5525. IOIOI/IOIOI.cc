fc729daf46cdf6a6eaff18a58fc1ebc34e86e0f1
Ushio-Hayase <166292944+Ushio-Hayase@users.noreply.github.com>
2024-07-12 19:08:37 +0900

    [Silver I] Title: IOIOI, Time: 8 ms, Memory: 3680 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, m; string str;
+	cin >> n >> m >> str;
+
+	int ans = 0;
+	for (int i = 0; i < m; i++) {
+		
+		int k = 0; 
+		if (str[i] == 'O') continue;
+
+		while (str[i + 1] == 'O' && str[i + 2] == 'I') {
+			k++;
+
+			if (k == n) {
+				ans++;
+				k--; 
+			}
+			i += 2; 
+		}
+	}
+
+	printf("%d\n", ans);
+
+}

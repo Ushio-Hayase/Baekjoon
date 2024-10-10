72c3b1e9e5645c9ab711e80409cb9c4a721495fa
Ushio-Hayase <166292944+Ushio-Hayase@users.noreply.github.com>
2024-10-10 09:46:06 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.rs
@@ -0,0 +1,57 @@
+use std::io::Error;
+
+static mut ARR: Vec<usize> = Vec::new();
+static mut DP: Vec<usize> = Vec::new();
+
+fn input() -> String {
+    std::io::stdin().lines().next().unwrap().unwrap()
+}
+
+fn search(index: usize, before_elem: usize) -> usize {
+    if unsafe { DP[index] } != 0 {
+        return unsafe { DP[index] };
+    }
+
+    let now_elem = unsafe { ARR[index] };
+
+    if now_elem <= before_elem {
+        return 0;
+    }
+
+    let mut res = 1;
+
+    for i in index + 1..unsafe { ARR.len() } {
+        let tmp = search(i, now_elem) + 1;
+
+        if res < tmp {
+            res = tmp;
+        }
+    }
+
+    unsafe {
+        DP[index] = res;
+    }
+    res
+}
+
+fn main() {
+    unsafe {
+        let dp_len: usize = input().parse().unwrap();
+        ARR = input()
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+        DP = vec![0; dp_len];
+    }
+
+    let mut res = 0;
+
+    for i in 0..unsafe { ARR.len() } {
+        let tmp = search(i, 0);
+        if res < tmp {
+            res = tmp;
+        }
+    }
+
+    println!("{res}");
+}

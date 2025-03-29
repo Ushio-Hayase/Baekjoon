8f6e89702c1fe0df83a04ede135225efb0a8906a
UshioHayase <166292944+Ushio-Hayase@users.noreply.github.com>
2025-03-29 19:42:23 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2692 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<vector<int>> ans;
+
+void func(vector<int> prevNumbers, int len)
+{
+    if (len >= M)
+    {
+        ans.push_back(prevNumbers);
+        return;
+    }
+
+    if (len == 0)
+    {
+        for (int i = 1; i <= N; ++i)
+        {
+            vector<int> tmp;
+            tmp.push_back(i);
+            func(tmp, len + 1);
+        }
+    }
+    else
+
+        for (int i = prevNumbers.back(); i <= N; ++i)
+        {
+            vector<int> tmp{prevNumbers};
+            tmp.push_back(i);
+            func(tmp, len + 1);
+        }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> M;
+
+    vector<int> x;
+    func(x, 0);
+
+    for (const auto& iter1 : ans)
+    {
+        for (const auto& iter2 : iter1) cout << iter2 << " ";
+        cout << "\n";
+    }
+}

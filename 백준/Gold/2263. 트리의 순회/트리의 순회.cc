068cdf9a2a46b92e7ddfd7efd85dcb2ec7012d5f
UshioHayase <166292944+Ushio-Hayase@users.noreply.github.com>
2025-06-14 13:25:15 +0900

    [Gold I] Title: 트리의 순회, Time: 100 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.cc
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.cc
@@ -1,24 +1,19 @@
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 int N;
-vector<int> inorder;
-vector<int> postorder;
-
-int find(int value, int start_idx, int end_idx)
-{
-    for (int i = start_idx; i <= end_idx; ++i)
-        if (inorder[i] == value) return i;
-}
+vector<int> inorder, postorder;
+unordered_map<int, int> pos;
 
 void construct(int inorder_start, int inorder_end, int postorder_start, int postorder_end)
 {
     if (inorder_start > inorder_end || postorder_start > postorder_end) return;
     int root = postorder[postorder_end];
 
-    int root_idx = find(root, inorder_start, inorder_end);
+    int root_idx = pos[root];
 
     cout << inorder[root_idx] << ' ';
 
@@ -32,7 +27,11 @@ int main()
     inorder.resize(N);
     postorder.resize(N);
 
-    for (int i = 0; i < N; ++i) cin >> inorder[i];
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> inorder[i];
+        pos[inorder[i]] = i;
+    }
     for (int i = 0; i < N; ++i) cin >> postorder[i];
 
     construct(0, N - 1, 0, N - 1);

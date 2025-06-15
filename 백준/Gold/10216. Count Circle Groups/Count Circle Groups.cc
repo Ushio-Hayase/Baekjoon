6444cf2b2c2a040e9fb85a5d91f1d86b43c9277a
UshioHayase <166292944+Ushio-Hayase@users.noreply.github.com>
2025-06-15 10:11:47 +0900

    [Gold IV] Title: Count Circle Groups, Time: 1588 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/10216. Count Circle Groups/Count Circle Groups.cc
+++ 백준/Gold/10216. Count Circle Groups/Count Circle Groups.cc
@@ -20,7 +20,7 @@ bool is_inside(Circle c1, Circle c2)
 
 int get_parent(vector<int>& parent, int x)
 {
-    if (parent[x] != x) return get_parent(parent, parent[x]);
+    if (parent[x] != x) return parent[x] = get_parent(parent, parent[x]);
     return parent[x];
 }
 
@@ -32,9 +32,9 @@ void union_parent(vector<int>& parent, int x, int y)
     if (root_x == root_y) return;
 
     if (root_x < root_y)
-        parent[y] = x;
+        parent[root_y] = root_x;
     else
-        parent[x] = y;
+        parent[root_x] = root_y;
 }
 
 int main()
@@ -45,7 +45,7 @@ int main()
         cin >> N;
         vector<Circle> arr(N);
         vector<int> parent(N);
-        set<int> result;
+        int result = N;
         for (int i = 0; i < N; ++i)
         {
             cin >> arr[i].x >> arr[i].y >> arr[i].r;
@@ -53,14 +53,13 @@ int main()
         }
 
         for (int i = 0; i < N; ++i)
-            for (int j = i; j < N; ++j)
-                if (is_inside(arr[i], arr[j])) union_parent(parent, parent[i], parent[j]);
+            for (int j = i + 1; j < N; ++j)
+                if (is_inside(arr[i], arr[j]) && get_parent(parent, i) != get_parent(parent, j))
+                {
+                    union_parent(parent, parent[i], parent[j]);
+                    result--;
+                }
 
-        for (int i = 0; i < N; ++i)
-        {
-            result.insert(get_parent(parent, parent[i]));
-        }
-
-        cout << result.size() << '\n';
+        cout << result << '\n';
     }
 }
